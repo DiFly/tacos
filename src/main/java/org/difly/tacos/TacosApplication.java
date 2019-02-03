48c9919616838e0e04e103d763963860a3e1df17
difly <kryshen.yaroslav@gmail.com>
2019-02-03 19:15:43 +0200

    Add initial commit

---
+++ src/main/java/org/difly/tacos/TacosApplication.java
@@ -0,0 +1,14 @@
+package org.difly.tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacosApplication.class, args);
+    }
+
+}
+
